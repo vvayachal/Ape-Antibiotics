32da988713261f61d0ab30391614c26e4a8a07ee
Aziz Hamza <azizhamza0@gmail.com>
2024-04-02 13:58:48 -0700

    minor adjustments
    removed empty script "combat.cs"

--- Assets/Scripts/Hittable.cs
+++ Assets/Scripts/Hittable.cs
@@ -9,7 +9,7 @@ public class Hittable : MonoBehaviour
 
     public void Hit(int damage, int knockback)
     {
-        onhit?.Invoke(damage,knockback);//if theres an event, calls the function and passes in damage and knockback
+        onhit?.Invoke(damage,knockback); // if theres an event, calls the function and passes in damage and knockback
     }
 }
 
