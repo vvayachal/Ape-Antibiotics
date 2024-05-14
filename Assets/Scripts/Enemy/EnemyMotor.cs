f29bf9c0ee023a3d1c8c81dec3ae746fc136d87c
Tegomlee <bryan.sanchez07@proton.me>
2024-05-13 20:18:19 -0400

    Removed some headers

--- Assets/Scripts/Enemy/EnemyMotor.cs
+++ Assets/Scripts/Enemy/EnemyMotor.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Serialization;
 
 public class EnemyMotor : MonoBehaviour
 {
